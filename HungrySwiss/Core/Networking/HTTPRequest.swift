496b4b7d915d2cb961d76b592957d234468dc334
Yoan Smit <yoan.smit@gmail.com>
2023-08-17 15:50:58 +0200

    feat(network): Add HTTPRequest protocol

---
+++ HungrySwiss/Core/Networking/HTTPRequest.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPRequest.swift
+//  HungrySwiss
+//
+//  Created by Yoan Smit on 17/08/2023.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+protocol HTTPRequest {
+    var method: HTTPMethod { get }
+    var path: String { get }
+    var queryParameters: [String: Any] { get }
+}
